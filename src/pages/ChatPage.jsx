d90d0fa700afad2e7de92ca6e3b5f9990bcbd1ee
d-huliu <huliud9vid@gmail.com>
2024-12-18 01:38:50 -0500

    netlify cant find path

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import TyceLogo from '../assets/TyceLogo.png'; 
+import TyceLogo from '@/assets/TyceLogo.png';
+
 import UserProfile from '../assets/chico.png'; 
 
 const ChatPage = () => {
